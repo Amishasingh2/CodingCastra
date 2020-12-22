0f398b84b3c227b760acb26e27962e5507965cb9
Amisha Singh <amisinghpatna@gmail.com>
2020-12-22 19:08:34 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = required('express');
+const app = express();
+const path = required('path');
+const forceSSL = function (){
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto']!== 'https')
+        {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+
+app.use(express.static(__dirname + '/dist/amishatutorial'));
+
+app.get('/', function(req,res)
+{
+    res.sendFile(path.join(__dirname + '/dist/amishatutorial/index.html'));
+
+});
+//Start the app by listening
+app.listen(process.env.PORT || 8080);
